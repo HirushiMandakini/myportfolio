35c9a16fb6397f5a009ab258713712fff8f2cadd
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-06-13 11:04:07 +0530

    UPDATED

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
     setTimeout(function() {
         document.body.classList.add('loaded'); // Add 'loaded' class to body after the specified loading time
-    }, 1000); // Adjust the loading time (in milliseconds) as needed
+    }, 2000); // Adjust the loading time (in milliseconds) as needed
 });
 
