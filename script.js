78d1aed8bbae3062d48a2f27ae2b5f57c20d16a9
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-06-15 14:51:10 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', function() {
     setTimeout(function() {
         document.body.classList.add('loaded'); // Add 'loaded' class to body after the specified loading time
+        
+        // Trigger animation by adding 'animate-slide' class to elements after a short delay
+        setTimeout(function() {
+            document.querySelector('.hometextdiv').classList.add('animate-slide');
+            document.querySelector('.homebtncontainer').classList.add('animate-slide');
+        }, 100); // Adjust delay as needed
     }, 2000); // Adjust the loading time (in milliseconds) as needed
 });
 
